44cfebf41fbc37eebecb4d5bc7c2e1e8a7007d86
Saed Sousa <saed.sousa@hotmail.com>
2024-06-08 15:17:02 -0300

    binary search tree

---
+++ dataStructure/src/main/MainBinarySearchTree.java
@@ -0,0 +1,32 @@
+package main;
+
+import datastructure.BinarySearchTree;
+
+public class MainBinarySearchTree {
+    public static void main(String[] args) {
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        myBST.insert(37);
+        myBST.insert(66);
+        myBST.insert(42);
+        myBST.insert(11);
+        myBST.insert(72);
+        myBST.insert(8);
+        myBST.insert(17);
+
+        myBST.inOrder();
+        System.out.println();
+
+        System.out.println(myBST.contains(37));
+        System.out.println(myBST.contains(36));
+        System.out.println(myBST.contains(66));
+        System.out.println(myBST.contains(8));
+        System.out.println(myBST.contains(100));
+        System.out.println();
+
+        myBST.deleteNode(66);
+        myBST.inOrder();
+
+
+    }
+}
